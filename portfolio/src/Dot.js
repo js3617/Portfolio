d5267a9a511d0620717e13afd0eed0f355351e01
js3617 <a332210@naver.com>
2024-01-05 03:15:23 +0900

    Dot 위치 수정

--- portfolio/src/Dot.js
+++ portfolio/src/Dot.js
@@ -1,44 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Dot = ({ num, scrollIndex, scrollToPage }) => {
+  const [isHovered, setIsHovered] = useState(false);
+
   const handleClick = () => {
     scrollToPage(num);
   };
 
   return (
-    <button
+    <div
       style={{
-        width: 6,
-        height: 6,
-        border: "white",
-        borderRadius: "50%",
-        backgroundColor: scrollIndex === num ? "aqua" : "white",
-        transition: "background-color 0.5s",
-        cursor: "pointer",
-        outline: "none",
+        marginLeft: 18,
+        width: 60,
+        height: 40,
+        display: 'flex',
+        alignItems: 'center',
+        color: 'white',
       }}
-      onClick={handleClick}
-      onMouseEnter={(e) => { 
-        e.target.style.transform = 'scale(1.5)';
-        e.target.style.backgroundColor = scrollIndex === num ? "aqua" : "white";
-      }}
-      onMouseLeave={(e) => { 
-        e.target.style.transform = 'scale(1)';
-        e.target.style.backgroundColor = scrollIndex === num ? "aqua" : "white";
-      }}
-    ></button>
+    >
+      <button
+        style={{
+          width: 5,
+          height: 5,
+          border: 'white',
+          borderRadius: '50%',
+          backgroundColor: scrollIndex === num ? '#17F997' : 'white',
+          transition: 'background-color 0.5s',
+          cursor: 'pointer',
+          outline: 'none',
+          transform: (isHovered || scrollIndex === num) ? 'scale(1.8)' : 'scale(1)',
+        }}
+        onClick={handleClick}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      ></button>
+      {isHovered && (
+        <div style={{ marginLeft: '7px', fontSize: '10px' }}>
+          {num === 1
+            ? 'Main'
+            : num === 2
+            ? 'About'
+            : num === 3
+            ? 'Work'
+            : num === 4
+            ? 'Contact'
+            : num === 5
+            ? 'iiiiii'
+            : ''}
+        </div>
+      )}
+    </div>
   );
 };
 
 const Dots = ({ scrollIndex, scrollToPage }) => {
   return (
-    <div style={{ position: "fixed", top: "50%", right: 100 }}>
+    <div style={{ position: 'fixed', top: '50%', left: 30, zIndex: 999 }}>
       <div
         style={{
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "space-between",
-          alignItems: "center",
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'space-between',
+          alignItems: 'center',
           width: 20,
           height: 100,
         }}
@@ -47,51 +70,10 @@ const Dots = ({ scrollIndex, scrollToPage }) => {
         <Dot num={2} scrollIndex={scrollIndex} scrollToPage={scrollToPage} />
         <Dot num={3} scrollIndex={scrollIndex} scrollToPage={scrollToPage} />
         <Dot num={4} scrollIndex={scrollIndex} scrollToPage={scrollToPage} />
+        <Dot num={5} scrollIndex={scrollIndex} scrollToPage={scrollToPage} />
       </div>
     </div>
   );
 };
 
 export default Dots;
-
-
-  // const Dot = ({ num, scrollIndex }) => {
-  //     return (
-  //       <div
-  //         style={{
-  //           width: 10,
-  //           height: 10,
-  //           border: "1px solid black",
-  //           borderRadius: 999,
-  //           backgroundColor: scrollIndex === num ? "black" : "transparent",
-  //           transitionDuration: 1000,
-  //           transition: "background-color 0.5s",
-  //         }}
-  //       ></div>
-  //     );
-  //   };
-    
-  //   const Dots = ({ scrollIndex }) => {
-  //     return (
-  //       <div style={{ position: "fixed", top: "50%", right: 100 }}>
-  //         <div
-  //           style={{
-  //             display: "flex",
-  //             flexDirection: "column",
-  //             justifyContent: "space-between",
-  //             alignItems: "center",
-  //             width: 20,
-  //             height: 100,
-  //           }}
-  //         >
-  //           <Dot num={1} scrollIndex={scrollIndex}></Dot>
-  //           <Dot num={2} scrollIndex={scrollIndex}></Dot>
-  //           <Dot num={3} scrollIndex={scrollIndex}></Dot>
-  //           <Dot num={3} scrollIndex={scrollIndex}></Dot>
-  //         </div>
-  //       </div>
-  //     );
-  //   };
-    
-  //   export default Dots;
-  
